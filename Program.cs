70b3095e429b2c537e0eeb93ae30a8f53edfd7e6
Michael Amudeshnikov <83592338+misha99fr@users.noreply.github.com>
2023-11-19 14:16:40 +0300

    Oaoaoaoaoaoaao

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold;
+
+public static class Program 
+{         
+    public static void Main()
+    {
+        Console.WriteLine("This program was ported from android c# shell!!?");
+    }
+}
